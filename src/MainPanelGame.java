ced6ef34dcf8684ae286df452dc98c98f10240a3
DzulhiRaihan <dzulhiraihan@gmail.com>
2023-12-10 15:27:47 +0800

    Second Progress

--- src/MainPanelGame.java
+++ src/MainPanelGame.java
@@ -19,7 +19,7 @@ public class MainPanelGame extends JPanel {
     boolean gameOver;
 
     public MouseAdapter myMouseListener;
-    private static final JButton NextLevelButton = new JButton("Next Level");
+    private static final JButton exitButton = new JButton("Exit");
     private boolean isNextLevelButtonPressed = false;
 
 
@@ -134,6 +134,42 @@ public class MainPanelGame extends JPanel {
         repaint();
     }
 
+    public void popUp(){
+        JFrame frame = new JFrame();
+        frame.setTitle("Slide Puzzle");
+        frame.setSize(300,150);
+        frame.setLocationRelativeTo(null);
+
+        JPanel panel = new JPanel();
+        frame.add(panel, "Center");
+        panel.setLayout(null);
+
+        JLabel label = new JLabel("You Win!");
+        label.setBounds(115, 20, 100, 10);
+        panel.add(label);
+
+        JButton playAgainButton = new JButton("Play Again");
+        playAgainButton.setBounds(30, 45, 100, 20);
+        panel.add(playAgainButton);
+
+        JButton exitButton = new JButton("Exit");
+        exitButton.setBounds(155, 45, 100, 20);
+        panel.add(exitButton);
+        frame.setVisible(true);
+
+        playAgainButton.addActionListener(e -> {
+            addMouseListener(myMouseListener);
+//            gameOver = false;
+            newGame();
+            frame.dispose();
+        });
+
+        exitButton.addActionListener(e -> {
+            new Level();
+            frame.dispose();
+        });
+    }
+
     private boolean isSolvable(){
         int countInversions = 0;
 
@@ -160,42 +196,6 @@ public class MainPanelGame extends JPanel {
         return true;
     }
 
-    private void popUp(){
-        JFrame frame = new JFrame();
-        frame.setTitle("Slide Puzzle");
-        frame.setSize(300,150);
-        frame.setLocationRelativeTo(null);
-
-        JPanel panel = new JPanel();
-        frame.add(panel, BorderLayout.CENTER);
-        panel.setLayout(null);
-
-        JLabel label = new JLabel("You Win!");
-        label.setBounds(115, 20, 100, 10);
-        panel.add(label);
-
-        JButton playAgainButton = new JButton("Play Again");
-        playAgainButton.setBounds(30, 45, 100, 20);
-        panel.add(playAgainButton);
-
-        NextLevelButton.setBounds(155, 45, 100, 20);
-        panel.add(NextLevelButton);
-        frame.setVisible(true);
-
-        playAgainButton.addActionListener(e -> {
-            addMouseListener(myMouseListener);
-            gameOver = false;
-            updateSize(3);
-            newGame();
-            frame.dispose();
-        });
-
-        NextLevelButton.addActionListener(e -> {
-
-
-        });
-    }
-
     private void drawGrid(Graphics2D g){
         for(int i = 0; i < tiles.length; i++){
             int r = i / size;
@@ -205,6 +205,7 @@ public class MainPanelGame extends JPanel {
 
             if(tiles[i] == 0){
                 if(gameOver){
+                    gameOver = false;
                     removeMouseListener(getMouseListeners()[0]);
                     popUp();
                 }
@@ -238,7 +239,7 @@ public class MainPanelGame extends JPanel {
     }
 
     public static void main(String[] args) {
-        MainPanelGame mainPanelGame = new MainPanelGame(10, 550, 30);
+        MainPanelGame mainPanelGame = new MainPanelGame(2, 550, 30);
         SwingUtilities.invokeLater(() -> {
             JFrame frame = new JFrame();
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
